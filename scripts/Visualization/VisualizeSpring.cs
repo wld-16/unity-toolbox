7b668330df2b9c3a40073492df38f5a925deb137
spacewave <wladimir.nabok@haw-hamburg.de>
2020-12-29 23:58:21 +0100

    Remove .meta files and added Spring visualization

---
+++ scripts/Visualization/VisualizeSpring.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class VisualizeSpring : MonoBehaviour
+{
+    private Vector3 springOriginPosition;
+    private Vector3 springEndPosition;
+
+    private LineRenderer _lineRenderer;
+    private SpringJoint _springJoint;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _springJoint = GetComponent<SpringJoint>();
+        springOriginPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        springOriginPosition = transform.TransformPoint(_springJoint.anchor);
+        springEndPosition = _springJoint.connectedBody.transform.TransformPoint(_springJoint.connectedAnchor);
+
+        _lineRenderer.SetPosition(0,springOriginPosition);
+        _lineRenderer.SetPosition(1,springEndPosition);
+    }
+}
